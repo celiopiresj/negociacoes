1abaee01b8f91a8d11b2306b9fffad3e74498fba
Celio Pires Jr. <72475635+celiopiresj@users.noreply.github.com>
2024-05-06 21:15:27 -0300

    template da tableta

--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -9,5 +9,10 @@ class Negociacoes {
 
     paraArray() {
         return [].concat(this._negociacoes);
-    }   
+    } 
+    
+    get volumeTotal() {
+        return this._negociacoes.reduce((total,negociacao) =>
+            total + negociacao.volume, 0)
+    }
 }
