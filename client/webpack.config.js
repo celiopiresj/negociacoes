c386ca39eb7b44e232d081d7fca68f965f929348
Celio Pires Jr. <72475635+celiopiresj@users.noreply.github.com>
2024-07-01 18:28:03 -0300

    teste final

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -42,7 +42,10 @@ plugins.push(
     )
 )
 
+let SERVICE_URL = JSON.stringify('http://localhost:3000')
+
 if (process.env.NODE_ENV == 'production'){
+    SERVICE_URL = JSON.stringify('http://api:3000')
     plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
     plugins.push(new babiliPlugin());
     plugins.push(new optimizeCSSAssentsPlugin({
@@ -55,6 +58,10 @@ if (process.env.NODE_ENV == 'production'){
         canPrint: true
     }));
 }
+
+plugins.push(new webpack.DefinePlugin({
+    SERVICE_URL
+}))
 
 module.exports = {
     entry: {
@@ -100,5 +107,8 @@ module.exports = {
             }
         ]
     },
-    plugins
+    plugins,
+    devServer: {
+        noInfo: true
+    }
 };
